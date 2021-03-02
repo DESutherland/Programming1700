53aee74314fbbdba62df1de6c25344147c8e79d8
Evan Sutherland <79775008+DESutherland@users.noreply.github.com>
2021-03-02 13:50:12 -0400

    Add files via upload
    A simple loan calculator. One of the first projects I wrote in PowerShell

---
+++ LoanCalculator.ps1
@@ -0,0 +1,55 @@
+<#
+Name: Evan Sutherland W0443868
+Date: 09/26/2020
+Description: Part two Assignment one for Prog1700
+#>
+
+# RENAME FUNCTION: Change the name of the function to match the file name for now.*
+function Get-WeeklyPayment {
+    
+    #BANNER
+    Write-Output "Weekly Loan Calculator"
+    Write-Output ""
+
+    #INPUT AND VARIABLES
+    $loanAmount = [float](Read-Host "Enter the amount of loan")
+    $interestRate = [float](Read-Host "Enter the interest rate")
+    $loanYear = [float](Read-Host "Enter the number of years")
+
+
+    #PROCESSING
+    $weeklyRate = $interestRate / 5200
+
+    #exponant = -52 * $loanYear
+    #Break equation down in to steps
+
+    # First step in 1 + the weekly rate we already calculated
+    $firstStep = 1 + $weeklyRate
+
+    # Second step is getting your value from Step one to the negative power of 52 times the number of loan years
+    $secondStep = [math]::POW($firstStep, -52 * $loanYear)
+
+    # Third step is subtracting the total of our second step from 1
+    $thirdStep = 1 - $secondStep
+
+    # Fourth step is dividing our weekly rate by the value of our third step
+    $fourthStep = $weeklyRate / $thirdStep
+
+    # Our final step is multiplying the value of our fourth step by the value of the loan amount
+    $weeklyPayment = $fourthStep * $loanAmount
+    
+
+    #OUTPUT    
+    
+    Write-Output ""
+    Write-Output ("The Weekly payment will be: {0:C}" -f $weeklyPayment)
+  
+}
+
+if ($MyInvocation.InvocationName -ne '.')
+{
+    # EDIT: Trigger our main function to launch the program by using the name of the function above
+    Get-WeeklyPayment
+}
+
+# * In IT, square brackets in an example generally mean they should be replaced along with text inside
